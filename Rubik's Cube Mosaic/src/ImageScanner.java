14e64bb6b3189febc967cf8999674e12c4e77de3
TXLonghornFan22 <TXLonghornFan22@gmail.com>
2018-11-11 18:58:46 -0600

    Update 1.1
    -Added a way to find out what color is the closest and puts it into a
    string array.
    -Started to make my GUI window to print out the final mosaic

--- Rubik's Cube Mosaic/src/ImageScanner.java
+++ Rubik's Cube Mosaic/src/ImageScanner.java
@@ -5,6 +5,8 @@ import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.util.Arrays;
 import java.lang.Math;
+import javax.swing.*;
+import java.awt.*;
 
 public class ImageScanner {
 	public static void main(String[] args) throws IOException {
@@ -75,17 +77,83 @@ public class ImageScanner {
 
 			}
 		}
-		System.out.print(imageR[164][170] + ", ");
-		System.out.print(imageG[164][170] + ", ");
-		System.out.print(imageB[164][170]);
+		//System.out.print(imageR[164][170] + ", ");
+		//System.out.print(imageG[164][170] + ", ");
+		//System.out.print(imageB[164][170]);
+		
 		x = 0;
 		y = 0;
+		double r; double g; double b;
+		double white; double yellow; double green; double blue; double red; double orange;
+		double min;
+		String newImage[][];
+		newImage = new String[height][width];
+		double average;
+		String color = null;
+		int windowSizex;
+		int windowSizey;
 		
-		while(x > width) {
-			Math.abs(CubeColor[0][0] - imageR[x][y]);
+		
+		JFrame window = new JFrame();
+		window.setTitle("Mosaic");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel panel = new JPanel();
+		Container pane = window.getContentPane();
+		window.setVisible(true);
+		
+		
+		
+		
+		while(x < width) {
+			r = Math.abs(CubeColor[0][0] - imageR[x][y]);
+			g = Math.abs(CubeColor[0][1] - imageG[x][y]);
+			b = Math.abs(CubeColor[0][2] - imageB[x][y]);
+			red = (r + g + b) / 3;
+			r = Math.abs(CubeColor[1][1] - imageR[x][y]);
+			g = Math.abs(CubeColor[1][2] - imageG[x][y]);
+			b = Math.abs(CubeColor[1][3] - imageB[x][y]);
+			orange = (r + g + b) / 3;
+			r = Math.abs(CubeColor[2][1] - imageR[x][y]);
+			g = Math.abs(CubeColor[2][2] - imageG[x][y]);
+			b = Math.abs(CubeColor[2][3] - imageB[x][y]);
+			green = (r + g + b) / 3;
+			r = Math.abs(CubeColor[3][1] - imageR[x][y]);
+			g = Math.abs(CubeColor[3][2] - imageG[x][y]);
+			b = Math.abs(CubeColor[3][3] - imageB[x][y]);
+			blue = (r + g + b) / 3;
+			r = Math.abs(CubeColor[4][1] - imageR[x][y]);
+			g = Math.abs(CubeColor[4][2] - imageG[x][y]);
+			b = Math.abs(CubeColor[4][3] - imageB[x][y]);
+			white = (r + g + b) / 3;
+			r = Math.abs(CubeColor[5][1] - imageR[x][y]);
+			g = Math.abs(CubeColor[5][2] - imageG[x][y]);
+			b = Math.abs(CubeColor[5][3] - imageB[x][y]);
+			yellow = (r + g + b) / 3;
+			
+			min = Math.min(red, Math.min(orange, (Math.min(green, Math.min(blue, Math.min(white, yellow))))));
+			
+			if(min == red) {
+				color = "Red";
+			}
+			else if(min == orange) {
+				color = "Orange";
+			}
+			else if(min == white) {
+				color = "White";
+			}
+			else if(min == yellow) {
+				color = "Yellow";
+			}
+			else if(min == blue) {
+				color = "Blue";
+			}
+			else if(min == green) {
+				color = "Green";
+			}
+			newImage[y][x] = color;
+			
+			// Take abs value (cube r, g, b subtracted by picture r, g, b). Then add the
+			// color rgb and which ever one is the lowest is what it becomes.
 		}
-		
-		// Take abs value (cube r, g, b subtracted by picture r, g, b). Then add the
-		// color rgb and which ever one is the lowest is what it becomes.
 	}
 }
